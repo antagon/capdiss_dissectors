efa8eab873d301424a4a95a8efa7c51def18d6e4
antagon <antagon@codeward.org>
2016-08-04 14:05:36 +0200

    New function get_stdout_type.

--- src/coroner.lua
+++ src/coroner.lua
@@ -1,7 +1,7 @@
 --- Main module.
 -- @module coroner
 
--- Enable colors if stdout is writting to terminal.
+-- Enable colors if stdout is writting to a terminal.
 if _STDOUT_TYPE == "chrdev" then
 	require ("color")
 else
@@ -27,15 +27,15 @@ function coroner.new_app (type)
 	return coroner.app:new (type)
 end
 
---- Get version of the Coroner Framework.
--- treturn string Version.
+--- Get version of the framework.
+-- @treturn string Version.
 function coroner.version ()
 	local ver = coroner_ver
 	return ver
 end
 
---- Get version of capdiss environment in which the script is running.
--- return Version string or nil, if the version could not be determined.
+--- Get version of capdiss environment.
+-- @return Version string or **nil**.
 function coroner.capdiss_version ()
 	if _CAPDISS_VERSION then
 		local _, _, ver = string.match (_CAPDISS_VERSION, "(%d.%d.%d)$")
@@ -43,6 +43,13 @@ function coroner.capdiss_version ()
 	end
 
 	return nil
+end
+
+--- Determine if, and how stdout was redirected (file/another program,...).
+-- @return One of the following values: _socket_, _file_,
+-- _chrdev_, _blkdev_, _fifo_, _unknown_ or **nil**.
+function coroner.get_stdout_type ()
+	return _STDOUT_TYPE
 end
 
 return coroner
