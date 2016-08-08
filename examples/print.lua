838bf981a693dfe56527861005bba8a26b75c791
antagon <antagon@codeward.org>
2016-08-09 00:08:24 +0200

    Example updated.

--- examples/print.lua
+++ examples/print.lua
@@ -4,31 +4,52 @@ local app = coroner.new_app (coroner.app.type.DISSECTOR)
 local hooks = {
 	-- Match an Ethernet frame...
 	eth = function (packet, ts, num)
-		print (("[%06d] %s :: %s -> %s"):format (num, packet:type ():upper (), packet:get_saddr (), packet:get_daddr ()))
+		print (("[%06d] %s :: %s => %s"):format (
+				num,
+				packet:type ():upper (),
+				packet:get_saddr ():color ("brblue", nil),
+				packet:get_daddr ():color ("brblue", nil)))
 	end,
 
 	-- Match an IP packet...
 	ip = function (packet, ts, num)
-		print (("[%06d] %s :: %s -> %s"):format (num, packet:type ():upper (), packet:get_saddr (), packet:get_daddr ()))
+		print (("[%06d] %s :: %s => %s"):format (
+				num,
+				packet:type ():upper (),
+				packet:get_saddr ():color ("brgreen", nil, true),
+				packet:get_daddr ():color ("brgreen", nil, true)))
 	end,
 
 	ipv6 = function (packet, ts, num)
-		print (("[%06d] %s :: %s -> %s"):format (num, packet:type ():upper (), packet:get_saddr (), packet:get_daddr ()))
+		print (("[%06d] %s :: %s => %s"):format (
+				num,
+				packet:type ():upper (),
+				packet:get_saddr ():color ("brgreen", nil, true),
+				packet:get_daddr ():color ("brgreen", nil, true)))
 	end,
 
 	-- Match a TCP packet...
 	tcp = function (packet, ts, num)
-		print (("[%06d] %s :: %s -> %s"):format (num, packet:type ():upper (), packet:get_srcport (), packet:get_dstport ()))
+		print (("[%06d] %s :: %s => %s"):format (
+				num,
+				packet:type ():upper (),
+				tostring (packet:get_srcport ()):color ("bryellow", nil),
+				tostring (packet:get_dstport ()):color ("bryellow", nil)))
 	end,
 
 	-- Match a UDP packet...
 	udp = function (packet, ts, num)
-		print (("[%06d] %s :: %s -> %s"):format (num, packet:type ():upper (), packet:get_srcport (), packet:get_dstport ()))
+		print (("[%06d] %s :: %s => %s"):format (num, packet:type ():upper (), packet:get_srcport (), packet:get_dstport ()))
 	end,
 
 	-- Trigger for each input file...
 	["@"] = function (filename, linktype)
 		print (("Reading %s (%s)..."):format (filename, linktype))
+	end,
+
+	-- Trigger after the last frame from input file was read...
+	["."] = function ()
+		print ("Done!")
 	end,
 
 	-- Trigger at the beginning of each frame...
