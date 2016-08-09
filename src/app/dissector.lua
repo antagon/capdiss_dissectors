89ba64d9ec66bc23326b6a66e145b9af675a145d
antagon <antagon@codeward.org>
2016-08-09 16:19:38 +0200

    Do not use set_hooks to set a signal handler.
    Instead a new method set_sigaction has been created for these purposes.

--- src/app/dissector.lua
+++ src/app/dissector.lua
@@ -1,5 +1,10 @@
 --- A dissector application.
--- @module dissector
+-- The module expects a table to be passed, populated with functions. Table
+-- keys match a protocol name or a special symbol. The module goes through the
+-- content of a capture file, frame by frame, dissecting individual packets
+-- inside of a frame. If a hook is defined for a matching protocol or for a
+-- special symbol, the hooked function is called.
+-- @classmod dissector
 local dissector = {}
 
 --- A list of supported link-types.
@@ -17,19 +22,28 @@ dissector.proto = {
 	icmp = true  -- Internet Control Message Protocol (ICMP)
 }
 
-function dissector.new ()
-	local new_diss = setmetatable ({}, { __index = dissector })
-
-	return new_diss
-end
-
 local function is_specialhook (name)
 	return name == "@" or name == "."
 		or name == "^" or name == "$"
-		or name == "*" or name == "sigaction"
+		or name == "*"
 end
 
---- Set a hook table.
+--- Set a hook table where the key names correspond to a protocol name or a symbol.
+--
+-- **@** (at sign) -- run at the beginning of each input file, before any
+-- frames are processed.
+--
+-- **.** (dot) -- run at the end of each input file, after all frames were processed.
+--
+-- **^** (caret) -- run for each frame before any packets inside the frame are
+-- processed.
+--
+-- **$** (dollar sign) -- run for each frame after all packets inside a frame
+-- were processed.
+--
+-- ***** (asterisk) -- run for any packet.
+--
+-- _protocol_ -- run for each occurence of a protocol matching the name _protocol_ (i.e. tcp).
 -- @tparam table hooks A key in a table is used to match a protocol name or a
 -- special hook name. The value stored in each key must be a function.
 -- @treturn boolean True on success, false on failure (error message is set).
@@ -46,13 +60,27 @@ function dissector:set_hooks (hooks)
 
 		if not is_specialhook (idx) then
 			if not dissector.proto[idx] then
-				self.errmsg = ("protocol '%s' not supported"):format (idx)
+				self.errmsg = ("protocol '%s' is not supported"):format (idx)
 				return false
 			end
 		end
 	end
 
 	dissector.usr_hook = hooks
+
+	return true
+end
+
+--- Set a function that should be called if a signal is delivered.
+-- @tparam function func Function to call.
+-- @treturn boolean True on success, false on failure (error message is set).
+function dissector:set_sigaction (func)
+	if type (func) ~= "function" then
+		self.errmsg = "signal action is not a function"
+		return false
+	end
+
+	self.usr_hook["sigaction"] = func
 
 	return true
 end
